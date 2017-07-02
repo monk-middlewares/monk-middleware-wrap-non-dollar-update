20a337ba107b4261a1b95ba431213496335031e1
Mathieu Dutour <mat.dutour@gmail.com>
2017-07-02 12:18:53 +0200

    fix indent

--- index.js
+++ index.js
@@ -1,10 +1,10 @@
 module.exports = function wrapNon$UpdateMiddleware (context) {
   return function (next) {
     return function (args, method) {
-			var wrap = (args.options || {}).wrapNon$UpdateField
-			if (typeof wrap !== 'undefined') {
-				delete args.options.wrapNon$UpdateField
-			}
+      var wrap = (args.options || {}).wrapNon$UpdateField
+      if (typeof wrap !== 'undefined') {
+        delete args.options.wrapNon$UpdateField
+      }
       if (wrap !== true || !args.update) {
         return next(args, method)
       }
